44cabc7c1b37d1ba3b882f16ebc4e76769a8380a
Becca <rkinnee91@hotmail.com>
2023-02-15 15:31:35 -0800

    built out functionality of input form

--- src/Components/AddItemForm/AddItemForm.js
+++ src/Components/AddItemForm/AddItemForm.js
@@ -2,32 +2,53 @@ import './AddItemForm.scss'
 import React from "react-dom";
 import { useState } from 'react';
 
+let nextId = 0;
 
 function AddItemForm() {
 
     const [userInput, setUserInput] = useState('');
+    const [items, setItems] = useState([]);
 
-    const handleChange  = (e) => {
-        setUserInput(e.currentTarget.value)
-    };
+    function handleClick() {
+        const insertAt= 0
+        const nextItems = [
+            ...items.slice(0, insertAt),
+            {id: nextId++, userInput: userInput},
+            ...items.slice(insertAt)
+        ];
+        setItems(nextItems);
+        setUserInput('')
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        AddItemForm(userInput)
-        setUserInput("")
-    };
+    }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <>
             <input 
                 value={userInput}
-                type="text"
-                onChange={handleChange}
+                onChange={e => setUserInput(e.target.value)}
                 placeholder = "add item"
                 className='form__container'
             />
-            <button className='form__button'>Add</button>
-        </form>
+            <button onClick={handleClick} className='form__button'>
+                Add
+            </button>
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>
+                        {item.userInput}{' '}
+                        <button onClick={ () => {
+                            setItems(
+                                items.filter(i => 
+                                    i.id !== item.id)
+                            )
+                        }}>
+                            X
+                        </button>
+                    
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
 
